587fbc1652a568ca890b7a83b2bd6a9ebe3ffdc1
Kevin Pickard <kevin.pickard@jumpcloud.com>
2016-10-13 15:25:26 -0600

    First pass on setup script

---
+++ .dotfiles/osxsetup.sh
@@ -0,0 +1,35 @@
+#/bin/bash -x
+
+# Script to setup a new macOS system. 
+# 	Author: Kevin Pickard
+#		Date: 10/13/16
+#			This script will install requirements for a macOS system (Tested on 10.12)
+#			Installs Xcode CLI Tools, Homebrew, Fish, and vim. 
+
+## Step 1: Install Xcode CLI Tools ##
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+PROD=$(softwareupdate -l |
+  grep "\*.*Command Line" |
+	head -n 1 | awk -F"*" '{print $2}' |
+	sed -e 's/^ *//' |
+	tr -d '\n')
+softwareupdate -i "$PROD" --verbose;
+
+## Step 2: Install Homebrew ##
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#		Update
+brew update
+
+## Step 3: Install Fish ##
+brew install fish
+
+## Step 4: Install vim ##
+brew install vim
+
+## Step 5: Clone github .dotfiles repo
+git clone --bare kevinjpickard/.dotfiles $HOME/.cfg
+alias dots='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+dots fetch
+dots checkout osx
+echo ".dotfiles" >> .gitignore
+dots config --local status.showUntrackedFiles no
