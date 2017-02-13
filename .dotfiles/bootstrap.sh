0b4e49f977c5ef17a07c4d91e23e873f163455ae
Kevin Pickard <kevin.pickard@jumpcloud.com>
2017-02-12 17:37:55 -0700

    cURL Linux Bootstrap script

--- .dotfiles/bootstrap.sh
+++ .dotfiles/bootstrap.sh
@@ -2,6 +2,7 @@
 OS=$(cat /etc/issue.net)
 if [[ $OS=='Ubuntu 16.04.2 LTS' || $OS=='Zorin OS 12'	]]
 then
+	curl https://raw.githubusercontent.com/kevinjpickard/.dotfiles/apt/.dotfiles/bootstrap_linux.sh -o ~/.dotfiles/bootstrap_linux.sh
 	sudo ~/.dotfiles/bootstrap_linux.sh
 elif [[ $(uname -a)=='Darwin*' ]]
 then 
