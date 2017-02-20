5b4822bf1c7ed7bc83c2f8a919fbb2c46589f2f1
Kevin Pickard <kevin.pickard@jumpcloud.com>
2017-02-19 18:41:10 -0700

    update gnome_settings

--- .dotfiles/gnome_settings.sh
+++ .dotfiles/gnome_settings.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
+if [ $DESKTOP_SESSION == 'gnome' ]; then
+	echo "GNOME DE detected, applying GNOME settings..."
+  
+  ## Installing GNOME extensions
+    # Dash to Dock
+    shell-extension-install `gnome-shell --version` 307
+    # Sensory Perception
+    shell-extension-install `gnome-shell --version` 1145
+    # Pixel Saver
+    shell-extension-install `gnome-shell --version` 723
+    # Transparent GNOME panel
+    shell-extension-install `gnome-shell --version` 1099
+    # CoverFlow alt-tab
+    shell-extension-install `gnome-shell --version` 97
+    # Multi-Monitors
+    shell-extension-install `gnome-shell --version` 921
+    # Workspaces to dock
+    shell-extension-install `gnome-shell --version` 427
+    # Docker Integration
+    shell-extension-install `gnome-shell --version` 1055
+    # Keys indicator
+    shell-extension-install `gnome-shell --version` 1105
+    # Random Wallpaper
+    shell-extension-install `gnome-shell --version` 1040
+    # Switcher
+    shell-extension-install `gnome-shell --version` 973
 
-## Installing GNOME extensions
-# Dash to Dock
-shell-extension-install `gnome-shell --version` 307
-# Sensory Perception
-shell-extension-install `gnome-shell --version` 1145
-# Pixel Saver
-shell-extension-install `gnome-shell --version` 723
-# Transparent GNOME panel
-shell-extension-install `gnome-shell --version` 1099
-# CoverFlow alt-tab
-shell-extension-install `gnome-shell --version` 97
-# Multi-Monitors
-shell-extension-install `gnome-shell --version` 921
-# Workspaces to dock
-shell-extension-install `gnome-shell --version` 427
-# Docker Integration
-shell-extension-install `gnome-shell --version` 1055
-# Keys indicator
-shell-extension-install `gnome-shell --version` 1105
-# Random Wallpaper
-shell-extension-install `gnome-shell --version` 1040
-# Switcher
-shell-extension-install `gnome-shell --version` 973
+  ## Restarting GNOME
+  gnome-shell --replace &
 
-## Restarting GNOME
-gnome-shell --replace &
+else 
+	echo "GNOME DE not detected."
+fi
