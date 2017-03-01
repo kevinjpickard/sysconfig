f2be03e91b6763fee10f4251bdf3c079441e4c18
Kevin Pickard <kevin.pickard@jumpcloud.com>
2017-03-01 10:32:36 -0700

    Try again

--- .dotfiles/gnome_extensions.sh
+++ .dotfiles/gnome_extensions.sh
@@ -1,46 +1,75 @@
 # Installing Gnome Shell Extensions
 mkdir $HOME/Documents/github/scratch/extensions
 ##	Dash To Dock
-wget -o $HOME/Documents/github/scratch/extensions/dashtodock.zip "https://extensions.gnome.org/download-extension/dash-to-dock@micxgx.gmail.com.shell-extension.zip?version_tag=6553" 
+wget "https://extensions.gnome.org/download-extension/dash-to-dock@micxgx.gmail.com.shell-extension.zip?version_tag=6553" 
+mv "$HOME/Documents/github/scratch/extensions/dash-to-dock@micxgx.gmail.com.shell-extension.zip?version_tag=6553" \
+ "$HOME/Documents/github/scratch/extensions/dash-to-dock@micxgx.gmail.com.shell-extension.zip?version_tag=6553.zip"
 mkdir -p "$HOME/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com"
-unzip -op $HOME/Documents/github/scratch/extensions/dashtodock.zip -d "$HOME/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com"
+unzip -o "$HOME/Documents/github/scratch/extensions/dash-to-dock@micxgx.gmail.com" -d "$HOME/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com"
 
 ##	Sensory Perception
-wget -o $HOME/Documents/github/scratch/extensions/sensoryperception.zip "https://extensions.gnome.org/download-extension/sensory-perception@HarlemSquirrel.github.io.shell-extension.zip?version_tag=6503" 
+wget "https://extensions.gnome.org/download-extension/sensory-perception@HarlemSquirrel.github.io.shell-extension.zip?version_tag=6503" 
+mv "$HOME/Documents/github/scratch/extensions/sensory-perception@HarlemSquirrel.github.io.shell-extension.zip?version_tag=6503" \
+	"$HOME/Documents/github/scratch/extensions/sensory-perception@HarlemSquirrel.github.io.shell-extension.zip?version_tag=6503.zip"
 mkdir -p "$HOME/.local/share/gnome-shell/extensions/sensory-perception@HarlemSquirrel.github.io"
-unzip -op $HOME/Documents/github/scratch/extensions/sensoryperception.zip -d "$HOME/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com"
+unzip -o "$HOME/Documents/github/scratch/extensions/sensory-perception@HarlemSquirrel.github.io.shell-extension.zip?version_tag=6503.zip" -d "$HOME/.local/share/gnome-shell/extensions/sensory-perception@HarlemSquirrel.github.io"
 
 ## Pixel Saver
-wget -o $HOME/Documents/github/scratch/extensions/pixelsaver.zip "https://extensions.gnome.org/download-extension/pixel-saver@deadalnix.me.shell-extension.zip?version_tag=6419" 
+wget "https://extensions.gnome.org/download-extension/pixel-saver@deadalnix.me.shell-extension.zip?version_tag=6419" 
+mv "$HOME/Documents/github/scratch/extensions/pixel-saver@deadalnix.me.shell-extension.zip?version_tag=6419" \
+	"$HOME/Documents/github/scratch/extensions/pixel-saver@deadalnix.me.shell-extension.zip?version_tag=6419.zip"
 mkdir -p "$HOME/.local/share/gnome-shell/extensions/pixel-saver@deadalnix.me"
-unzip -op $HOME/Documents/github/scratch/extensions/pixelsaver.zip -d "$HOME/.local/share/gnome-shell/extensions/pixel-saver@deadalnix.me"
+unzip -o "$HOME/Documents/github/scratch/extensions/pixel-saver@deadalnix.me.shell-extension.zip?version_tag=6419.zip" -d "$HOME/.local/share/gnome-shell/extensions/pixel-saver@deadalnix.me"
 
 ## Transparent GNOME Panel
-wget -o $HOME/Documents/github/scratch/extensions/transpanel.zip "https://extensions.gnome.org/download-extension/transparent-gnome-panel@ttomovcik.com.shell-extension.zip?version_tag=6056" 
+wget "https://extensions.gnome.org/download-extension/transparent-gnome-panel@ttomovcik.com.shell-extension.zip?version_tag=6056" 
+mv "$HOME/Documents/github/scratch/extensions/transparent-gnome-panel@ttomovcik.com.shell-extension.zip?version_tag=6056" \
+	"$HOME/Documents/github/scratch/extensions/transparent-gnome-panel@ttomovcik.com.shell-extension.zip?version_tag=6056.zip"
 mkdir -p "$HOME/.local/share/gnome-shell/extensions/transparent-gnome-panel@ttomovcik.com"
-unzip -op $HOME/Documents/github/scratch/extensions/transpanel.zip -d "$HOME/.local/share/gnome-shell/extensions/transparent-gnome-panel@ttomovcik.com"
+unzip -o "$HOME/Documents/github/scratch/extensions/transparent-gnome-panel@ttomovcik.com.shell-extension.zip?version_tag=6056.zip" -d "$HOME/.local/share/gnome-shell/extensions/transparent-gnome-panel@ttomovcik.com"
 
 ## CoverFlow Alt-Tab
-wget -o $HOME/Documents/github/scratch/extensions/coverflow.zip "https://extensions.gnome.org/download-extension/CoverflowAltTab@palatis.blogspot.com.shell-extension.zip?version_tag=6328" 
+wget "https://extensions.gnome.org/download-extension/CoverflowAltTab@palatis.blogspot.com.shell-extension.zip?version_tag=6328" 
+mv "$HOME/Documents/github/scratch/extensions/CoverflowAltTab@palatis.blogspot.com.shell-extension.zip?version_tag=6328" \
+	"$HOME/Documents/github/scratch/extensions/CoverflowAltTab@palatis.blogspot.com.shell-extension.zip?version_tag=6328.zip"
 mkdir -p "$HOME/.local/share/gnome-shell/extensions/CoverflowAltTab@palatis.blogspot.com"
-unzip -op $HOME/Documents/github/scratch/extensions/coverflow.zip -d "$HOME/.local/share/gnome-shell/extensions/CoverflowAltTab@palatis.blogspot.com"
+unzip -o "$HOME/Documents/github/scratch/extensions/CoverflowAltTab@palatis.blogspot.com.shell-extension.zip?version_tag=6328.zip" -d "$HOME/.local/share/gnome-shell/extensions/CoverflowAltTab@palatis.blogspot.com"
 
 ## Multi-Monitors
-wget -o $HOME/Documents/github/scratch/extensions/monitors.zip "https://extensions.gnome.org/download-extension/multi-monitors-add-on@spin83.shell-extension.zip?version_tag=6235" 
+wget "https://extensions.gnome.org/download-extension/multi-monitors-add-on@spin83.shell-extension.zip?version_tag=6235" 
+mv "$HOME/Documents/github/scratch/extensions/multi-monitors-add-on@spin83.shell-extension.zip?version_tag=6235" \
+	"$HOME/Documents/github/scratch/extensions/multi-monitors-add-on@spin83.shell-extension.zip?version_tag=6235.zip"
 mkdir -p "$HOME/.local/share/gnome-shell/extensions/multi-monitors-add-on@spin83"
-unzip -op $HOME/Documents/github/scratch/extensions/monitors.zip -d "$HOME/.local/share/gnome-shell/extensions/multi-monitors-add-on@spin83"
+unzip -o "$HOME/Documents/github/scratch/extensions/multi-monitors-add-on@spin83.shell-extension.zip?version_tag=6235.zip" -d "$HOME/.local/share/gnome-shell/extensions/multi-monitors-add-on@spin83"
 
 ## Docker Integration
-wget -o $HOME/Documents/github/scratch/extensions/docker.zip "https://extensions.gnome.org/download-extension/docker-integration@jan.trejbal.gmail.com.shell-extension.zip?version_tag=6340" 
+wget "https://extensions.gnome.org/download-extension/docker-integration@jan.trejbal.gmail.com.shell-extension.zip?version_tag=6340" 
+mv "$HOME/Documents/github/scratch/extensions/docker-integration@jan.trejbal.gmail.com.shell-extension.zip?version_tag=6340" \
+	"$HOME/Documents/github/scratch/extensions/docker-integration@jan.trejbal.gmail.com.shell-extension.zip?version_tag=6340.zip"
 mkdir -p "$HOME/.local/share/gnome-shell/extensions/docker-integration@jan.trejbal.gmail.com"
-unzip -op $HOME/Documents/github/scratch/extensions/docker.zip -d "$HOME/.local/share/gnome-shell/extensions/docker-integration@jan.trejbal.gmail.com"
+unzip -o "$HOME/Documents/github/scratch/extensions/docker-integration@jan.trejbal.gmail.com.shell-extension.zip?version_tag=6340.zip" -d "$HOME/.local/share/gnome-shell/extensions/docker-integration@jan.trejbal.gmail.com"
 
 ## Keys Indicator
-wget -o $HOME/Documents/github/scratch/extensions/keys.zip "https://extensions.gnome.org/download-extension/keys-indicator@caasiu.github.com.shell-extension.zip?version_tag=6488" 
+wget "https://extensions.gnome.org/download-extension/keys-indicator@caasiu.github.com.shell-extension.zip?version_tag=6488" 
+mv "$HOME/Documents/github/scratch/extensions/keys-indicator@caasiu.github.com.shell-extension.zip?version_tag=6488" \
+	"$HOME/Documents/github/scratch/extensions/keys-indicator@caasiu.github.com.shell-extension.zip?version_tag=6488.zip"
 mkdir -p "$HOME/.local/share/gnome-shell/extensions/keys-indicator@caasiu.github.com"
-unzip -op $HOME/Documents/github/scratch/extensions/keys.zip -d "$HOME/.local/share/gnome-shell/extensions/keys-indicator@caasiu.github.com"
+unzip -o "$HOME/Documents/github/scratch/extensions/keys-indicator@caasiu.github.com.shell-extension.zip?version_tag=6488.zip" -d "$HOME/.local/share/gnome-shell/extensions/keys-indicator@caasiu.github.com"
 
 ## Random Wallpaper
-wget -o $HOME/Documents/github/scratch/extensions/wallpaper.zip "https://extensions.gnome.org/download-extension/randomwallpaper@iflow.space.shell-extension.zip?version_tag=6560" 
+wget "https://extensions.gnome.org/download-extension/randomwallpaper@iflow.space.shell-extension.zip?version_tag=6560" 
+mv "$HOME/Documents/github/scratch/extensions/randomwallpaper@iflow.space.shell-extension.zip?version_tag=6560" \
+	"$HOME/Documents/github/scratch/extensions/randomwallpaper@iflow.space.shell-extension.zip?version_tag=6560.zip"
 mkdir -p "$HOME/.local/share/gnome-shell/extensions/randomwallpaper@iflow.space"
-unzip -op $HOME/Documents/github/scratch/extensions/keys.zip -d "$HOME/.local/share/gnome-shell/extensions/randomwallpaper@iflow.space"
+unzip -o "$HOME/Documents/github/scratch/extensions/randomwallpaper@iflow.space.shell-extension.zip?version_tag=6560.zip" -d "$HOME/.local/share/gnome-shell/extensions/randomwallpaper@iflow.space"
+
+## Enable Extensions
+gnome-shell-extension-tool -e dash-to-dock@micxgx.gmail.com 
+gnome-shell-extension-tool -e sensory-perception@HarlemSquirrel.github.io 
+gnome-shell-extension-tool -e pixel-saver@deadalnix.me 
+gnome-shell-extension-tool -e transparent-gnome-panel@ttomovcik.com 
+gnome-shell-extension-tool -e CoverflowAltTab@palatis.blogspot.com 
+gnome-shell-extension-tool -e multi-monitors-add-on@spin83 
+gnome-shell-extension-tool -e docker-integration@jan.trejbal.gmail.com 
+gnome-shell-extension-tool -e keys-indicator@caasiu.github.com 
+gnome-shell-extension-tool -e randomwallpaper@iflow.space
