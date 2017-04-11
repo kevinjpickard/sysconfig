230d9f2a90b245414e2e4dd65fa373cbae94f7fb
Kevin Pickard <kevin.pickard@jumpcloud.com>
2017-04-11 09:18:15 -0600

    Updated hs config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -8,8 +8,11 @@ hotkey.bind(mash, "j", function() tiling.cycle(1) end)
 hotkey.bind(mash, "k", function() tiling.cycle(-1) end)
 hotkey.bind(mash, "space", function() tiling.promote() end)
 hotkey.bind(mash, "f", function() tiling.goToLayout("fullscreen") end)
-hotkey.bind(mash, ".", function() hs.spotify.next() end)
+
+-- Spotify Hotkeys --
+hotkey.bind(mash, ".", function() hs.spotify.next() hs.spotify.displayCurrentTrack() end)
 hotkey.bind(mash, ",", function() hs.spotify.previous() end)
+hotkey.bind(mash, "/", function() hs.spotify.displayCurrentTrack() end)
 
 -- If you want to set the layouts that are enabled
 tiling.set('layouts', {
@@ -23,8 +26,6 @@ end)
 hs.alert.show("Config loaded")
 
 -- Lock
-hs.hotkey.bind({"cmd", "ctrl"}, 'L', function()
-    os.execute("open '/System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app'")
-end)
+hotkey.bind(mash, "l", function() hs.caffeinate.lockScreen() end)
 
 require('aerosnap')
