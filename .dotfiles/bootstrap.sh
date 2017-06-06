07b6b868a85ec122a0d479074f80a72a1afc70b1
Kevin Pickard <kevin.pickard@jumpcloud.com>
2017-06-06 13:10:50 -0600

    Gettin real tired of your shit

--- .dotfiles/bootstrap.sh
+++ .dotfiles/bootstrap.sh
@@ -2,7 +2,7 @@
 
 os=$(uname)
 
-if ( "$os" == "Darwin" ); then
+if [[ $os == "Darwin" ]]; then
   echo "Detected macOS, calling Darwin setup scripts..."
   # Install homebrew
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -31,7 +31,7 @@ if ( "$os" == "Darwin" ); then
   ~/.dotfiles/osxsetup.sh
 fi
 
-if ( "$os" == "Linux" ); then
+if [[ $os == "Linux" ]]; then
   # Linux setup
   echo "linux detected, calling Linux setup scripts..."
   sudo apt-get install git -y
