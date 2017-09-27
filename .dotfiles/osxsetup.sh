34e4bc4899e4a5e9bb20e79f6ac1643ef7f0b4dd
Kevin Pickard <kevin.pickard@jumpcloud.com>
2017-09-27 14:09:22 -0600

    updates

--- .dotfiles/osxsetup.sh
+++ .dotfiles/osxsetup.sh
@@ -27,6 +27,7 @@ git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 ## ZSH Completion Suggestions 
+export ZSH_CUSTOM=~/.oh-my-zsh/custom
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 ## Install dein
--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 My system config files
 
 ```
-curl https://raw.githubusercontent.com/kevinjpickard/.dotfiles/osx/.dotfiles/bootstrap.sh | bash
+curl https://raw.githubusercontent.com/kevinjpickard/.dotfiles/master/.dotfiles/bootstrap.sh | bash
 ```
