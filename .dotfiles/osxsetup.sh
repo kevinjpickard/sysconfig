6674d61ac3a487806907d3aa61bc61255c4c2330
Kevin Pickard <kevin.pickard@jumpcloud.com>
2017-09-28 15:07:45 -0600

    Trying to get past the softwareupdate bug...

--- .dotfiles/osxsetup.sh
+++ .dotfiles/osxsetup.sh
@@ -11,9 +11,11 @@ echo "Installing a lot of shit..."
 brew cask install java
 brew tap homebrew/bundle
 brew bundle --file=~/.dotfiles/Brewfile
+curl https://bootstrap.pypa.io/get-pip.py | bash
 
 ## Install Powerline
 #	Install Powerline
+pip install powerline-status
 pip3 install powerline-status
 #	Install powerline fonts
 mkdir ~/scratch/fonts
@@ -27,8 +29,7 @@ git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 ## ZSH Completion Suggestions 
-export ZSH_CUSTOM=~/.oh-my-zsh/custom
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone git://github.com/zsh-users/zsh-autosuggestions ${zsh_custom:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 ## Install dein
 echo "Installing dein..."
