2f513e444d103c5f3f7f8c7818b9827f6b779c7f
Kevin Pickard <kevin.pickard@jumpcloud.com>
2018-02-25 17:10:16 -0700

    Some fixes, vim setup

--- .dotfiles/arch_setup.sh
+++ .dotfiles/arch_setup.sh
@@ -12,9 +12,6 @@ sudo locale-gen
 echo "LANGUAGE=en_US
 LANG=en_US.UTF-8
 LC_ALL=C" | sudo tee -a /etc/locale.conf
-echo 'Hostname?'
-read INPUT
-echo $INPUT | sudo tee /etc/hostname
 
 ## Install Yaourt
 mkdir -p ~/Documents/github.com/scratch
@@ -49,4 +46,27 @@ services=$(<~/.dotfiles/arch_enabled_services)
 for service in $services; do
 	systemctl enable $service
 done
+
+## Setting up ZSH
+# Clone oh-my-zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+# Syntax Highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# ZSH Autosuggestions
+git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# Change Shell
+chsh -s `which zsh`
+
+## Install dein
+echo "Installing dein..."
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ~/installer.sh
+sh ./installer.sh ~/.config/nvim/dein
+echo "Setting up folder structure..."
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config/nvim}
+ln -s ~/.config/nvim ~/.vim
+ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+echo "Setting up neovim..."
+# molokai color scheme
+mkdir ~/.vim/colors
+curl https://raw.githubusercontent.com/tamelion/neovim-molokai/master/colors/molokai.vim -o ~/.vim/colors/molokai.vim
 
