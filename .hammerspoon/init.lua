c3b47f984988c8c351fdef3968fac9d9e83ad420
Kevin Pickard <kevin.pickard@jumpcloud.com>
2018-05-29 12:35:58 -0600

    Updated hs config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,7 +1,15 @@
--- Tiling Init
+-- System Variables --
+hs.window.animationDuration = 0 -- Turn off window animation, its smoother
+
+-- Tiling Init -- 
 local tiling = require "hs.tiling"
 local hotkey = require "hs.hotkey"
 local mash = {"ctrl", "cmd"}
+
+-- If you want to set the layouts that are enabled --
+tiling.set('layouts', {
+  'fullscreen', 'main-vertical'
+})
 
 hotkey.bind(mash, "c", function() tiling.cycleLayout() end)
 hotkey.bind(mash, "j", function() tiling.cycle(1) end)
@@ -15,14 +23,14 @@ hotkey.bind(mash, ",", function() hs.spotify.previous() end)
 hotkey.bind(mash, "/", function() hs.spotify.displayCurrentTrack() end)
 hotkey.bind(mash, "p", function() hs.spotify.playpause() end)
 
--- If you want to set the layouts that are enabled
-tiling.set('layouts', {
-  'fullscreen', 'main-vertical'
-})
+-- Volume Control --
+local volumeIncrement = 5
+hotkey.bind(mash, "[", function() hs.audiodevice.defaultOutputDevice():setVolume(hs.audiodevice.current().volume - volumeIncrement) end)
+hotkey.bind(mash, "]", function() hs.audiodevice.defaultOutputDevice():setVolume(hs.audiodevice.current().volume + volumeIncrement) end)
 
 -- Reload hammerspoon configs
 hs.hotkey.bind({"cmd", "ctrl"}, "R", function()
-    hs.reload()
+  hs.reload()
 end)
 hs.alert.show("Config loaded")
 
