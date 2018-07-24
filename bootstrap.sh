3ab4d89504ae1afc2b05659fca9e318f8b7b541e
Kevin Pickard <kevin.pickard@jumpcloud.com>
2018-07-24 00:30:15 -0600

    add yay and stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,13 @@
-sudo pacman -Sy ansible --noconfirm
-ansible-playbook --connection=local /tmp/kitchen/.dotfiles/arch.yml
+# for some reason, when this suite is run on an Arch Linux host pacman errors out due 
+# to some conflicting files and I don't feel like finding a proper fix right now so
+# TODO: Do better.
+if [[ -d /usr/include/rpcsvc ]]; then
+  sudo rm -rf /usr/include/rpcsvc
+fi
+
+if [[ -e /usr/lib/libnsl.so ]]; then
+  sudo rm /usr/lib/libnsl.so
+fi
+
+sudo pacman -Sy --noconfirm ansible
+ansible-playbook --connection=local -vvv /tmp/kitchen/.dotfiles/arch.yml
