5cfc8d3e4b1c2bee89d8d97f3eb3ec5eeaf912ab
Kevin Pickard <kevinjpickard@gmail.com>
2018-08-03 22:55:55 -0600

    Some more progress, also more problems.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -x
+
 # for some reason, when this suite is run on an Arch Linux host pacman errors out due 
 # to some conflicting files and I don't feel like finding a proper fix right now so
 # TODO: Do better.
@@ -10,8 +12,10 @@ if [[ -e /usr/lib/libnsl.so ]]; then
 fi
 
 # Using command -v for POSIX compatibility
-if [[ !$(command -v ansible-playbook) ]]; then
-  echo 'Ansible not installed, installing now...'
+if command -v ansible-playbook > /dev/null; then
+  echo 'Ansible is already installed'
+else
+  echo 'Ansible is not installed, installing now...'
 	sudo pacman -Sy --noconfirm --needed ansible
 fi
 
