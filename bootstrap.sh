6f72ef98fbf9899444be1c5a37adf2b9320da210
Kevin Pickard <kevin.pickard@jumpcloud.com>
2018-08-14 14:09:27 -0600

    Updated bootstrap and added macos tasks

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,16 +11,32 @@ if [[ -e /usr/lib/libnsl.so ]]; then
   sudo rm /usr/lib/libnsl.so
 fi
 
-# Using command -v for POSIX compatibility
-if command -v ansible-playbook > /dev/null; then
-  echo 'Ansible is already installed'
+if [[ $OSTYPE == darwin* ]]; then
+  echo "Detected MacOS"
+  echo "Installing HomeBrew..."
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+  echo "Brew installed. Installing Ansible..."
+  brew install ansible
+
+  echo "Ansible installed. Cloning files..."
+  brew install git
+  git clone https://github.com/kevinjpickard/.dotfiles.git ~/.dotfiles
+
+  echo "Executing playbook..."
+  ansible-playbook --connection=local ~/.dotfiles/core.yml
 else
-  echo 'Ansible is not installed, installing now...'
-	sudo pacman -Sy --noconfirm --needed ansible
+  # Using command -v for POSIX compatibility
+  if command -v ansible-playbook > /dev/null; then
+    echo 'Ansible is already installed'
+  else
+    echo 'Ansible is not installed, installing now...'
+    sudo pacman -Syyu --noconfirm --needed ansible
+  fi
+
+  if [[ -e /tmp/kitchen/core.retry ]]; then
+    ANSIBLE_LIBRARY="/tmp/kitchen/library/aur:$ANSIBLE_LIBRARY" ansible-playbook --connection=local /tmp/kitchen/core.yml --extra-vars "username=kevin hostname=KJP-test" --limit @/tmp/kitchen/core.retry -vvv
+  else
+    ANSIBLE_LIBRARY="/tmp/kitchen/library/aur:$ANSIBLE_LIBRARY" ansible-playbook --connection=local /tmp/kitchen/core.yml --extra-vars "username=kevin hostname=KJP-test nvidia=true" -vvv
 fi
 
-if [[ -e /tmp/kitchen/core.retry ]]; then
-	ANSIBLE_LIBRARY="/tmp/kitchen/library/aur:$ANSIBLE_LIBRARY" ansible-playbook --connection=local /tmp/kitchen/core.yml --extra-vars "username=kevin hostname=KJP-test" --limit @/tmp/kitchen/core.retry -vvv
-else
-	ANSIBLE_LIBRARY="/tmp/kitchen/library/aur:$ANSIBLE_LIBRARY" ansible-playbook --connection=local /tmp/kitchen/core.yml --extra-vars "username=kevin hostname=KJP-test nvidia=true" -vvv
-fi
