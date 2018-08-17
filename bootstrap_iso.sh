665c8198dc72f4b8eb25d5e8841b38a63f31b6ef
Kevin Pickard <kevin.pickard@jumpcloud.com>
2018-08-17 14:27:47 -0600

    First pass packer template and scripts

---
+++ bootstrap_iso.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/zsh
+pacman -Sy ansible git
+git clone -b arch https://github.com/kevinjpickard/.dotfiles.git
+ansible --version
+#ansible-playbook --connection=local .dotfiles/arch_setup.yml --extra-vars "username=kevin hostname=KJP-test"
+
+# example of how to run a playbook in a chroot
+#sudo ansible-playbook -c chroot -i '/path/to/debootstrap,/path/to/febootstrap' my-cool-playbook.yml
+
