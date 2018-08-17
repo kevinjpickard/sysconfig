c37d4e3513181ec9e1722524aed3f269f39e13dc
Kevin Pickard <kevin.pickard@jumpcloud.com>
2018-08-17 14:41:26 -0600

    Update script and packer template

--- bootstrap_iso.sh
+++ bootstrap_iso.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
-sudo pacman -Sy ansible git
+sudo pacman -Sy --noconfirm ansible git
 git clone -b arch https://github.com/kevinjpickard/.dotfiles.git
 ansible --version
 #ansible-playbook --connection=local .dotfiles/arch_setup.yml --extra-vars "username=kevin hostname=KJP-test"
