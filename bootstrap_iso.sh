00075ce7fb31ca96b176d5792fab9b916dd22520
Kevin Pickard <kevinjpickard@gmail.com>
2018-08-18 20:08:42 -0600

    try this

--- bootstrap_iso.sh
+++ bootstrap_iso.sh
@@ -2,7 +2,6 @@
 sudo pacman -Sy --noconfirm python ansible git
 git clone -b arch https://github.com/kevinjpickard/.dotfiles.git
 python --version
-/usr/bin/ansible --version
 ansible-playbook --connection=local .dotfiles/core.yml
 # ansible-playbook --connection=local .dotfiles/arch_setup.yml --extra-vars "username=kevin hostname=KJP-test"
 
