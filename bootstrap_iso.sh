9cede6913027261e6f0e80cefa01129b7dbe382b
Kevin Pickard <kevinjpickard@gmail.com>
2018-08-18 20:03:27 -0600

    trying some stuff

--- bootstrap_iso.sh
+++ bootstrap_iso.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/zsh
 sudo pacman -Sy --noconfirm ansible git
 git clone -b arch https://github.com/kevinjpickard/.dotfiles.git
-ansible --version
-#ansible-playbook --connection=local .dotfiles/arch_setup.yml --extra-vars "username=kevin hostname=KJP-test"
+python --version
+/usr/bin/ansible --version
+ansible-playbook --connection=local .dotfiles/core.yml
+# ansible-playbook --connection=local .dotfiles/arch_setup.yml --extra-vars "username=kevin hostname=KJP-test"
 
 # example of how to run a playbook in a chroot
 #sudo ansible-playbook -c chroot -i '/path/to/debootstrap,/path/to/febootstrap' my-cool-playbook.yml
-
