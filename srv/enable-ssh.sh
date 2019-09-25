903eb6deb7af5753b235c2a3a880acda94ae18e9
kevinjpickard <kevinjpickard@gmail.com>
2019-09-24 23:23:17 -0600

    unix line endings for ssh script

--- srv/enable-ssh.sh
+++ srv/enable-ssh.sh
@@ -8,4 +8,3 @@ echo 'Defaults env_keep += "SSH_AUTH_SOCK"' > /etc/sudoers.d/10_vagrant
 echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/10_vagrant
 /usr/bin/chmod 0440 /etc/sudoers.d/10_vagrant
 /usr/bin/systemctl start sshd.service
-
