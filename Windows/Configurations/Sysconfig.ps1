ec7ef035eb1c219c1d6ac263e1d4256efd46009c
Kevin Pickard <kevin.pickard@jumpcloud.com>
2020-01-19 23:16:34 -0700

    some updates

--- Windows/Configurations/Sysconfig.ps1
+++ Windows/Configurations/Sysconfig.ps1
@@ -18,30 +18,124 @@ Configuration Sysconfig
       InstallDir = "c:\choco"
     }
 
-    cChocoPackageInstallerSet installOtherDevDependencies {
-      Ensure    = 'Present'
-      Name      = @(
-        "7zip",
-        "Firefox",
-        "Spotify",
-        "discord",
-        "geforce-experience",
-        "git",
-        "golang",
-        "GoogleChrome",
-        "jq",
-        "pia",
-        "python",
-        "slack",
-        "sublimetext3",
-        "sysinternals",
-        "vagrant",
-        "veracrypt",
-        "virtualbox",
-        "vlc",
-        "vscode"
-      )
-      DependsOn = "[cChocoInstaller]installChoco"
+    # cChocoPackageInstallerSet installApps {
+    #   Ensure = 'Present'
+    #   Name   = @(
+    #     "7zip",
+    #     "Firefox",
+    #     "discord",
+    #     "geforce-experience",
+    #     "git",
+    #     "golang",
+    #     "GoogleChrome",
+    #     "jq",
+    #     "pia",
+    #     "python",
+    #     "slack",
+    #     "sublimetext3",
+    #     "sysinternals",
+    #     "vagrant",
+    #     "veracrypt",
+    #     "virtualbox",
+    #     "vlc",
+    #     "vscode"
+    #   )
+    #   # DependsOn = "[cChocoInstaller]installChoco"
+    # }
+
+    # cChocoPackageInstaller Install_Spotify {
+    #   Ensure = 'Present'
+    #   Name   = 'Spotify'
+    # }
+
+    cChocoPackageInstaller Install_7Zip {
+      Ensure = 'Present'
+      Name   = '7zip'
+    }
+
+    cChocoPackageInstaller Install_Firefox {
+      Ensure = 'Present'
+      Name   = 'Firefox'
+    }
+
+    # cChocoPackageInstaller Install_Discord {
+    #   Ensure = 'Present'
+    #   Name   = 'Discord'
+    # }
+
+    cChocoPackageInstaller Install_GeForceExperience {
+      Ensure = 'Present'
+      Name   = 'geforce-experience'
+    }
+
+    cChocoPackageInstaller Install_Git {
+      Ensure = 'Present'
+      Name   = 'git'
+    }
+
+    cChocoPackageInstaller Install_Golang {
+      Ensure = 'Present'
+      Name   = 'golang'
+    }
+
+    cChocoPackageInstaller Install_GoogleChrome {
+      Ensure = 'Present'
+      Name   = 'GoogleChrome'
+    }
+
+    cChocoPackageInstaller Install_JQ {
+      Ensure = 'Present'
+      Name   = 'jq'
+    }
+
+    cChocoPackageInstaller Install_PIA {
+      Ensure = 'Present'
+      Name   = 'pia'
+    }
+
+    cChocoPackageInstaller Install_Python {
+      Ensure = 'Present'
+      Name   = 'python'
+    }
+
+    cChocoPackageInstaller Install_Slack {
+      Ensure = 'Present'
+      Name   = 'slack'
+    }
+
+    cChocoPackageInstaller Install_SublimeText3 {
+      Ensure = 'Present'
+      Name   = 'sublimetext3'
+    }
+
+    cChocoPackageInstaller Install_Sysinternals {
+      Ensure = 'Present'
+      Name   = 'sysinternals'
+    }
+
+    cChocoPackageInstaller Install_Vagrant {
+      Ensure = 'Present'
+      Name   = 'vagrant'
+    }
+
+    cChocoPackageInstaller Install_Veracrypt {
+      Ensure = 'Present'
+      Name   = 'veracrypt'
+    }
+
+    cChocoPackageInstaller Install_VirtualBox {
+      Ensure = 'Present'
+      Name   = 'virtualbox'
+    }
+
+    cChocoPackageInstaller Install_VLC {
+      Ensure = 'Present'
+      Name = 'vlc'
+    }
+
+    cChocoPackageInstaller Install_VSCode {
+      Ensure = 'Present'
+      Name = 'vscode'
     }
   }
 }
